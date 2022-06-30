eacba75ea43caf1fd45b235847226d4619b622b2
Vika6329 <102681400+Vika6329@users.noreply.github.com>
2022-06-30 18:43:24 +0300

    Delete app directory

--- app/js/main.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-  $('.menu-btn, .header-menu__close').on('click', function () {
-    $('.menu').toggleClass('menu--active');
-    $('.menu-btn').toggleClass('menu-btn--active');
-    $('body').toggleClass('lock');
-  });
-});
